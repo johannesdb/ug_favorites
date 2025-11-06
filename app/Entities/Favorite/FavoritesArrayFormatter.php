ed074bba4154149a18a7964538cf91a058d9a765
johannesdb <johannes@alfanova.dk>
2025-11-06 01:00:24 +0100

    Fix der faktisk virker

--- app/Entities/Favorite/FavoritesArrayFormatter.php
+++ app/Entities/Favorite/FavoritesArrayFormatter.php
@@ -7,8 +7,6 @@ use Favorites\Entities\FavoriteList\FavoriteList;
 
 /**
 * Format the user's favorite array to include additional post data
-* OPTIMIZED: Removed unused data (title, permalink, thumbnails, excerpt, button, total)
-* to reduce response size and eliminate expensive postmeta queries
 */
 class FavoritesArrayFormatter
 {
@@ -16,6 +14,11 @@ class FavoritesArrayFormatter
 	* Formatted favorites array
 	*/
 	private $formatted_favorites;
+
+	/**
+	* Total Favorites Counter
+	*/
+	private $counter;
 
 	/**
 	* Post ID to add to return array
@@ -32,11 +35,16 @@ class FavoritesArrayFormatter
 	private $site_id;
 
 	/**
-	* Status for post to add to array
+	* Site ID for post to add to array
 	* For adding/removing session/cookie favorites for current request
 	* @var string
 	*/
 	private $status;
+
+	public function __construct()
+	{
+		$this->counter = new FavoriteCount;
+	}
 
 	public function format($favorites, $post_id = null, $site_id = null, $status = null)
 	{
@@ -45,32 +53,45 @@ class FavoritesArrayFormatter
 		$this->site_id = $site_id;
 		$this->status = $status;
 		$this->resetIndexes();
+		$this->addPostData();
 		return $this->formatted_favorites;
 	}
 
 	/**
-	* Reset the favorite indexes and reverse order
+	* Reset the favorite indexes
 	*/
 	private function resetIndexes()
 	{
-		$this->checkCurrentPost();
-
 		foreach ( $this->formatted_favorites as $site => $site_favorites ){
+			// Make older posts compatible with new name
 			if ( !isset($site_favorites['posts']) ) {
 				$site_favorites['posts'] = $site_favorites['site_favorites'];
 				unset($this->formatted_favorites[$site]['site_favorites']);
 			}
 			foreach ( $site_favorites['posts'] as $key => $favorite ){
-				// If $favorite is already an array (from checkCurrentPost), use the post_id from it
-				if ( is_array($favorite) ) {
-					$post_id = $favorite['post_id'];
-					unset($this->formatted_favorites[$site]['posts'][$key]);
-					$this->formatted_favorites[$site]['posts'][$post_id] = $favorite;
-				} else {
-					// Legacy format: $favorite is the post ID
-					unset($this->formatted_favorites[$site]['posts'][$key]);
-					$this->formatted_favorites[$site]['posts'][$favorite]['post_id'] = $favorite;
-				}
+				unset($this->formatted_favorites[$site]['posts'][$key]);
+				$this->formatted_favorites[$site]['posts'][$favorite]['post_id'] = $favorite;
+			}
+		}
+	}
+
+	/**
+	* Add the post type to each favorite
+	*/
+	private function addPostData()
+	{
+		$this->checkCurrentPost();
+		foreach ( $this->formatted_favorites as $site => $site_favorites ){
+			foreach ( $site_favorites['posts'] as $key => $favorite ){
+				$site_id = $this->formatted_favorites[$site]['site_id'];
+				$this->formatted_favorites[$site]['posts'][$key]['post_type'] = get_post_type($key);
+				$this->formatted_favorites[$site]['posts'][$key]['title'] = get_the_title($key);
+				$this->formatted_favorites[$site]['posts'][$key]['permalink'] = get_the_permalink($key);
+				$this->formatted_favorites[$site]['posts'][$key]['total'] = $this->counter->getCount($key, $site_id);
+				$this->formatted_favorites[$site]['posts'][$key]['thumbnails'] = $this->getThumbnails($key);
+				$this->formatted_favorites[$site]['posts'][$key]['excerpt'] = apply_filters('the_excerpt', get_post_field('post_excerpt', $key));
+				$button = new FavoriteButton($key, $site_id);
+				$this->formatted_favorites[$site]['posts'][$key]['button'] = $button->display(false);
 			}
 			$this->formatted_favorites[$site] = array_reverse($this->formatted_favorites[$site]);
 		}
@@ -93,5 +114,21 @@ class FavoritesArrayFormatter
 				}
 			}
 		}
+	}
+
+	/**
+	* Add thumbnail urls to the array
+	*/
+	private function getThumbnails($post_id)
+	{
+		if ( !has_post_thumbnail($post_id) ) return false;
+		$sizes = get_intermediate_image_sizes();
+		$thumbnails = [];
+		foreach ( $sizes as $size ){
+			$url = get_the_post_thumbnail_url($post_id, $size);
+			$img = '<img src="' . $url . '" alt="' . get_the_title($post_id) . '" class="favorites-list-thumbnail" />';
+			$thumbnails[$size] = apply_filters('favorites/list/thumbnail', $img, $post_id, $size);
+		}
+		return $thumbnails;
 	}
 }
